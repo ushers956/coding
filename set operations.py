4adf5f67fec82a3ed12ff91e7266f4d30faca05d
ushers956 <ushershaninz@gmail.com>
2025-02-10 19:39:05 +1300

    set

---
+++ set operations.py
@@ -0,0 +1,18 @@
+my_set = {1, 2, 3}
+print(my_set)
+
+my_set = {1.0, "hello", (1, 2, 3)}
+print(my_set)
+
+my_set = {1, 2, 3, 4, 3, 2}
+print(my_set)
+
+my_set = set({1,2,3,2})
+print(my_set,"/n")
+
+num_set = set([0,1,2,3,4,5])
+print("orignal set:")
+print(num_set)
+num_set.pop()
+print("after removeing the first element from the said set:")
+print(num_set,"/n")
