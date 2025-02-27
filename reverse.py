6d74ad2eec2e8406135e2bbde01c382f00655c37
ushers956 <ushershaninz@gmail.com>
2025-02-27 20:03:35 +1300

    reverse

---
+++ reverse.py
@@ -0,0 +1,14 @@
+class class_reverse:
+
+    def __init__(self, word_s):
+        self.s = word_s
+
+    def reverse_word(self):
+        return self.s [::-1]
+    
+
+
+word = input("enter the word to be reversed : ")
+rev_ob = class_reverse(word)
+result = rev_ob.reverse_word()
+print("reverse string: ", result)
