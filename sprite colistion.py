61dd71e325dbac6d24cb3b854efad57801b3a799
ushers956 <ushershaninz@gmail.com>
2025-03-20 20:05:59 +1300

    sprite

--- sprite colistion.py
+++ sprite colistion.py
@@ -27,4 +27,49 @@ class sprite(pygame. sprite. Sprite):
                 min(self.rect.y + y_change,SCREEN_HEIGHT - self.rect.width), 0)
             
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption
+pygame.display.set_caption("Sprite Collision")
+all_sprites = pygame.sprite.Group()
+
+sprite1 = Sprite(pygame.Color('black'), 20, 30)
+sprite1.rect.x, sprite1.rect.y = random.randint(
+    0, SCREEN_WIDTH - sprite1.rect.width), random.randint(
+        0, SCREEN_HEIGHT - sprite1.rect.height)
+all_sprites.add(sprite1)
+
+sprite2 = Sprite(pygame.Color('red'), 20, 30)
+sprite2.rect.x, sprite2.rect.y = random.randint(
+    0, SCREEN_WIDTH - sprite2.rect.width), random.randint(
+        0, SCREEN_HEIGHT - sprite2.rect.height)
+all_sprites.add(sprite2)
+
+running, won = True, False
+clock = pygame.time.Clock()
+
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN
+                                     and event.key == pygame.K_x):
+      running = False
+
+  if not won:
+    keys = pygame.key.get_pressed()
+    x_change = (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * MOVEMENT_SPEED
+    y_change = (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * MOVEMENT_SPEED
+    sprite1.move(x_change, y_change)
+
+    if sprite1.rect.colliderect(sprite2.rect):
+      all_sprites.remove(sprite2)
+      won = True
+
+
+    screen.blit(background_image, (0, 0))
+    all_sprites.draw(screen)
+    if won:
+    win_text = font.render("You win!", True, pygame.Color('black'))
+    screen.blit(win_text, ((SCREEN_WIDTH - win_text.get_width()) // 2,
+                           (SCREEN_HEIGHT - win_text.get_height()) // 2))
+
+  pygame.display.flip()
+  clock.tick(90)
+
+pygame.quit()
