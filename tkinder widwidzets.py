a8de2b4acc9e04ce5b6db4595bae42d7f3477e39
ushers956 <ushershaninz@gmail.com>
2025-04-02 19:20:28 +1300

    tkinter

--- tkinder widwidzets.py
+++ tkinder widwidzets.py
@@ -21,3 +21,23 @@ def display():
     global message
     message = "welcome to the application \n todays date is: "
     greet = "hello"+name+"\n"
+
+
+
+    text_box.insert(END, greet)
+    text_box.insert(END, message)
+    text_box.insert(END, date.today())
+
+
+text_box = Text(height=3)
+
+
+btn = Button(text="Begin", command=display, height=1, bg="#1261A0", fg='white')
+
+
+
+lbl.pack()
+name_lbl.pack()
+name_entry.pack()
+btn.pack()
+text_box.pack()
