f867893b160a6b94e7cafa1ab1ef7ed82d67652a
ushers956 <ushershaninz@gmail.com>
2025-04-30 18:49:39 +1200

    top window

--- text editer.py
+++ text editer.py
@@ -37,4 +37,15 @@ def save_file():
         output_file.write(text)
     window.title(f"codingals text editer - {filepath}")
 
-txt_edit
+txt_edit = Text(window)
+fr_buttons = Frame(window, relief=RAISED, bd=2)
+btn_open = Button(fr_buttons, text="Open", command=open_file)
+btn_save = Button(fr_buttons, text="Save As...", command=save_file)
+
+btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+btn_save.grid(row=1, column=0, sticky="ew", padx=5)
+
+fr_buttons.grid(row=0, column=0, sticky="ns")
+txt_edit.grid(row=0, column=1, sticky="nsew")
+
+window.mainloop()
