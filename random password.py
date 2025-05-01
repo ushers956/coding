87d0f7105012a166a1c36bf17f4577db68cc98f8
ushers956 <ushershaninz@gmail.com>
2025-05-01 18:56:07 +1200

    restaurant

---
+++ random password.py
@@ -0,0 +1,8 @@
+import random 
+import string
+
+print('hello, welcome to random password genorater')
+length = int(input('\nenter the length of password: '))
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
