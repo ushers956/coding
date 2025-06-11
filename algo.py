435244a062b38b112eec11dd6b6cab5b136867f9
ushers956 <ushershaninz@gmail.com>
2025-06-11 18:52:27 +1200

    algo

---
+++ algo.py
@@ -0,0 +1,23 @@
+def fun1(n):
+
+    return n*(n+1)/2
+print(fun1(4))
+
+print("so, the number of iteration will be 1 for any input")
+def fun2(n)
+    
+    sun=0
+
+    for i in range(1,n+1):
+
+
+        sum+=i
+
+
+    return sum
+
+
+print(fun2(4))
+
+
+print("so, number of iteration will be 1 + 1 + 1 + 1 = 4 = n(input) iterations")
