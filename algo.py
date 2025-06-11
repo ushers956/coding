99ebf377dd87c9604ee32b255169b65d52af66f3
ushers956 <ushershaninz@gmail.com>
2025-06-11 19:00:54 +1200

    algo

--- algo.py
+++ algo.py
@@ -4,9 +4,9 @@ def fun1(n):
 print(fun1(4))
 
 print("so, the number of iteration will be 1 for any input")
-def fun2(n)
+def fun2(n):
     
-    sun=0
+    sum=0
 
     for i in range(1,n+1):
 
@@ -21,3 +21,21 @@ print(fun2(4))
 
 
 print("so, number of iteration will be 1 + 1 + 1 + 1 = 4 = n(input) iterations")
+
+
+
+def fun3(n):
+
+
+    sum=0
+
+
+    for i in range(1,n+1):
+
+        for j in range(1,i+1)
+            sum+=1
+
+    return sum
+print(fun3(4))
+
+print("number of iteration will be 1 + 2 + 3 + 4 = 10")
