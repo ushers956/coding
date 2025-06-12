b40f7e6b04efd02b4dde5e22c890fc39d95b97c0
ushers956 <ushershaninz@gmail.com>
2025-06-12 18:37:28 +1200

    time

---
+++ O(1)time.py
@@ -0,0 +1,10 @@
+def printnumber(n):
+    iteration=0
+    print("the number entered by user is",n)
+    iteration+=1
+    print("total iteration done by code is", iteration,"\n")
+    
+
+printnumber(10)
+printnumber(20)
+print("\n with any 'n'the time taken by our code wont change")
