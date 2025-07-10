6c895196e5cc1dcb226468230cb033777f5d01bd
ushers956 <ushershaninz@gmail.com>
2025-07-10 19:02:06 +1200

    homework

---
+++ bit wise homework.py
@@ -0,0 +1,25 @@
+num1 = 12
+num2 = 6
+
+print("num1 & num2 =", num1 & num2)
+
+print("\nnum1 | num2 =", num1 | num2)
+
+print("\n~num1 =", ~num1)
+
+print("\nnum1 ^ num2 =",num1 ^ num2)
+
+
+num1 = 12
+num2 = 6
+
+print("\nnum1 >> 1 =", num1 >> 1)
+
+print("\nnum2 >> 1 =", num2 >> 1)
+
+num1 = 12
+num2 = 6
+
+print("\nnum1 << 1 =", num1 << 1)
+
+print("\nnum2 << 1 =", num2 << 1)
