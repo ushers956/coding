306e226d60601530a26d17e4731771641327cf60
ushers956 <ushershaninz@gmail.com>
2025-07-31 18:47:06 +1200

    factorial

---
+++ factorial.py
@@ -0,0 +1,9 @@
+# program to find factorial of any number using recursion
+def fac(n):
+    # when n is 1 or 0, return 1
+    if(n==1 or n==0):
+        return 1
+    #factorial of n = n*n-1*n-2...1
+    return n*fac(n-1)
+n = int(input("enter your number :"))
+print ("factorial of", n,"is :",fac(n))
