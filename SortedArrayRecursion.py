fd117ebdedb30bc472ebb0c1a7a8b0400d9cd190
ushers956 <ushershaninz@gmail.com>
2025-08-07 18:33:48 +1200

    SortedArrayRecursion

---
+++ SortedArrayRecursion.py
@@ -0,0 +1,16 @@
+def checkSorted(a):
+
+    length = len(a)
+
+    if length == 1 or length == 0:
+        return True
+    
+    return a[0] <= a[1] and checkSorted(a[1:])
+
+a = [1,2,3,5,6,8,2]
+
+if checkSorted(a):
+
+    print("\nYes given array is sorted")
+else:
+    print("\nNo given array is not sorted")
