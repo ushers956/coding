6c58f6ec806f7b26569d33e65476081c56b63f6f
ushers956 <ushershaninz@gmail.com>
2025-08-13 18:37:44 +1200

    reverseNumber

---
+++ reverse number.py
@@ -0,0 +1,11 @@
+def reverseNumber(num):
+    if(num > 0):
+
+        last = num%10
+        if(num//10>0):
+            current_number = reverseNumber((int)(num // 10))
+            return last*pow(10,len(str(current_number))) + current_number
+        return num
+    
+n = int(input("enter your number : "))
+print("reversed : ",reverseNumber(n))
